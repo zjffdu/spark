880614d08e45c8c60a9ad13a5c0dcf283e994a62
jerryshao <sshao@hortonworks.com>
2015-12-03 16:52:56 +0800

    BUG-48982 Add hadoop lzo jar to the classpath
    (cherry picked from commit 3548004419041700987b0095a37459f1d6c3fbc5)

--- launcher/src/main/java/org/apache/spark/launcher/AbstractCommandBuilder.java
+++ launcher/src/main/java/org/apache/spark/launcher/AbstractCommandBuilder.java
@@ -228,6 +228,12 @@ abstract class AbstractCommandBuilder {
     addToClassPath(cp, getenv("YARN_CONF_DIR"));
     addToClassPath(cp, getenv("SPARK_DIST_CLASSPATH"));
 
+    // Add Hadoop lzo jar to classpath
+    String lzo = getenv("_HADOOP_LZO_JAR");
+    if (lzo != null) {
+      addToClassPath(cp, lzo);
+    }
+
     // Add Azure wasb paths only on linux sku
     if (isHumboldt() && !isWindows()){
       try {
